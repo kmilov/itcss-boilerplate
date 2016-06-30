699e46dec357624aa5c8c42b7acdea9a75535f04
Camilo Vasco <jcastano@xumak.com>
2016-06-30 10:59:16 -0500

    SC5 + SCSS sample project

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # ITCSS boilerplate
+
+This is a sample project using SASS with ITCSS, Bliss standards and
+a living style guide with SC5
+
+## How to run
+
+```sh
+npm install
+gulp
+```
+
+The gulp tasks will compile the sass files and copy the compiled file to public/css
+also, will run the SC5 styleguide tool
+
+## URLS
+
+```sh
+http://localhost:3000   - public/ instance
+http://localhost:3002   - browser sync instance
+http://localhost:3003   - living styleguide
+```
+
+## ITCSS standard pyramid implementation sample
+
+According to ITCSS, all files should reside in one folder
+in this case, we split every layer in different folders.
+but, we added created the file names with the appropiated layer
+
+## Structure
+
+### SASS
+the styles used for this living style guide is under app/assets/stylesheets
+the entry point is application.scss, this is in charge of load every ITCSS layer
+and also, create the top level indexes for the style guide in the KSS syntax
+
+##
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,43 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var styleguide = require('sc5-styleguide');
+var styleguideBuildPath = "public/styleguide";
+
+gulp.task('styleguide:generate', function(){
+	return gulp.src('app/assets/stylesheets/**/*.scss')
+		.pipe(styleguide.generate({
+			title: 'ITCSS Styleguide sample',
+			server: true,
+			rootPath: styleguideBuildPath,
+			overviewPath: 'README.md',
+			sideNav: true,
+			port: 3003,
+		}))
+		.pipe(gulp.dest(styleguideBuildPath));
+})
+
+gulp.task('styleguide:applystyles', function(){
+	return gulp.src('app/assets/stylesheets/application.scss')
+		.pipe(sass({
+			errLogToConsole: true
+		}))
+		.pipe(styleguide.applyStyles())
+		.pipe(gulp.dest(styleguideBuildPath));
+})
+
+gulp.task('styleguide:assets', function() {
+  gulp.src(['app/assets/fonts/**'])
+    .pipe(gulp.dest(styleguideBuildPath + '/fonts'));
+});
+
+gulp.task('styleguide', ['styleguide:generate', 'styleguide:assets', 'styleguide:applystyles'])
 
 gulp.task('sass', function(){
 	return gulp.src('app/assets/stylesheets/**/*.scss')
-	.pipe(sass())
+	.pipe(sass({
+		errLogToConsole: true
+	}))
 	.pipe(gulp.dest('./public/css'))
 	.pipe(browserSync.stream())
 });
@@ -14,8 +47,8 @@ gulp.task('serve', function(){
 		server: "./public"
 	});
 
-	gulp.watch("app/assets/stylesheets/**/*.scss", ["sass"]);
+	gulp.watch("app/assets/stylesheets/**/*.scss", ["sass", "styleguide"]);
 	gulp.watch("public/**/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['styleguide', 'serve']);
